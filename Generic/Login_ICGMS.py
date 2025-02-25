2cc0a7b10d939e3bb6ea23788241c5fcacca6d2a
Parth Grover <groverparth19@gmail.com>
2025-02-25 16:35:55 +0530

    removed extra imports

--- Generic/Login_ICGMS.py
+++ Generic/Login_ICGMS.py
@@ -1,10 +1,5 @@
 # import os ; import sys
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.webdriver import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 import time
 from selenium import webdriver
 from Generic.BaseClassICGMS import *
