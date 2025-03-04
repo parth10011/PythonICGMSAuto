43c79de854da50cc59b5cf1b87c91d59f4ae9b6c
parth10011 <groverparth19@gmail.com>
2025-03-04 13:11:58 +0530

    Initiate updated

--- Generic/InitiateNewProcess_details.py
+++ Generic/InitiateNewProcess_details.py
@@ -5,6 +5,7 @@ url_UAT = "https://uat.icgms.insapp.in/"
 url_Local = "http://192.168.32.28:4200/"
 emailer_url = "https://release-api.icgms.sharajman.com/emailer"
 Doc_Link = "C:/Users/parth/OneDrive/Desktop/DTU/Dummy.pdf"
+Doc2_Link = "C:/Users/Parth Grover/Desktop/DTU/Dummy.pdf"
 
 a1dmin = "superadmnphase1@owleyes.ch" ; P2ass= "Admin123"
 
