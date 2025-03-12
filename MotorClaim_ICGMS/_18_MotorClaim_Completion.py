dfdf83887d45082f8494c1253ad94eedcd128cb7
parth10011 <groverparth19@gmail.com>
2025-03-12 13:34:26 +0530

    17 and 18 steps of motor claim completed

---
+++ MotorClaim_ICGMS/_18_MotorClaim_Completion.py
@@ -0,0 +1,36 @@
+import time
+from Generic.BaseClassICGMS import *
+from Generic.Login_ICGMS import *
+from selenium.webdriver.common.by import By
+
+# Login
+setUp()
+time.sleep(1)
+Login_ICGMS.login(a1dmin , P2ass)
+ToasterPopupClick()
+
+# Open View List
+view_list = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/claim-details']")))
+view_list.click()
+
+# Completion Button Click
+complete_button = wait.until(EC.element_to_be_clickable((By.XPATH, "(//button[text()='Completion'])[1]")))
+complete_button.click()
+
+# Enter Date of Delivery of vehicle
+dateDelivery_datepick = wait.until((EC.presence_of_element_located((By.ID, "vehicle_delivery"))))
+dateDelivery_datepick.send_keys(current_date)
+
+# Select Process Completed radio button (either yes or no)
+processComplete_radio = driver.find_elements(By.XPATH, "//input[@value='yes'] | //input[@value='no']")
+random.choice(processComplete_radio).click()
+
+# Click on Submit Button
+submit_button = driver.find_element(By.XPATH, "//span[text()='Submit']")
+submit_button.click()
+ToasterPopupClick()
+
+# Open Claim View List
+view_list = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/claim-details']")))
+view_list.click()
+logOut()
