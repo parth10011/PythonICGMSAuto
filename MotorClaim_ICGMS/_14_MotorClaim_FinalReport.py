430dac5c7298affecbf9216fba19c1a9f2b11adc
parth10011 <groverparth19@gmail.com>
2025-03-12 12:30:56 +0530

    new 12-16 steps of Motor Claim added and little changes in interface file

---
+++ MotorClaim_ICGMS/_14_MotorClaim_FinalReport.py
@@ -0,0 +1,87 @@
+import time
+import keyboard
+from selenium.webdriver.support.ui import Select
+from Generic.BaseClassICGMS import *
+from Generic.Login_ICGMS import *
+from selenium.webdriver.common.by import By
+
+# Login
+setUp()
+time.sleep(1)
+Login_ICGMS.login(Surveyor_Email1 , P2ass)
+
+ToasterPopupClick()
+time.sleep(0.5)
+
+# Open Surveyor Claim View List
+survClaimList_link = wait.until((EC.presence_of_element_located((By.XPATH, "//a[@href='/surveyor-claim-list']"))))
+survClaimList_link.click()
+
+# Click on Final Report button
+finalReport_button = wait.until((EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'Final Report')][1]"))))
+finalReport_button.click()
+
+# Upload Final Report
+finalReport_fileupload = driver.find_element(By.XPATH, "(//input[@type='file'])[1]")
+finalReport_fileupload.send_keys(Est_Report)
+
+# Enter Remark
+remark_text = driver.find_element(By.XPATH, "//textarea[@placeholder='Enter Your Remark']")
+remark_text.send_keys("Final Report Uploaded Successfuly")
+
+# Upload Document file
+docFile_fileupload = driver.find_element(By.XPATH, "(//input[@type='file'])[2]")
+docFile_fileupload.send_keys(Doc2_Link)
+
+# Enter File Name
+fileName_text = driver.find_element(By.XPATH, "//input[@placeholder='Enter filename']")
+fileName_text.send_keys("File98234y")
+
+# Set Browser to Mobile View
+driver.set_window_size(765,1000)
+
+# Close Menu
+driver.find_element(By.XPATH, "//i[@class='ti-close']").click()
+
+# Scroll to the particular web element of page
+Image = driver.find_element(By.XPATH, "(//input[@accept='application/pdf'])[2]")
+driver.execute_script("arguments[0].scrollIntoView();", Image)
+time.sleep(1)
+
+# 
+driver.find_element(By.XPATH, "//label[@for='cameraInput0']").click()
+time.sleep(1)
+keyboard.press_and_release("esc")
+time.sleep(1)
+driver.maximize_window()
+
+# 
+image_fileupload = driver.find_element(By.XPATH, "//input[@accept='image/*']")
+image_fileupload.send_keys(Img_Link)
+ToasterPopupClick()
+time.sleep(1)
+
+# View Customer Inspection Report
+ViewCustReport = wait.until((EC.visibility_of_element_located((By.XPATH, "//button[text()=' View ']"))))
+ViewCustReport.click()
+ToasterPopupClick()
+
+# Close Customer Inspection Report
+close_button = wait.until((EC.visibility_of_element_located((By.XPATH, "//button[@aria-label='Close']"))))
+close_button.click()
+
+# Download Customer Inspection Report
+DownCustReport = driver.find_element(By.XPATH, "//button[text()=' Download ']")
+DownCustReport.click()
+ToasterPopupClick()
+time.sleep(2)
+
+# Click on Submit Button
+submit_button = driver.find_element(By.XPATH, "//span[text()='Submit']")
+submit_button.click()
+
+# Open Surveyor Claim View List
+survClaimList_link = wait.until((EC.presence_of_element_located((By.XPATH, "//a[@href='/surveyor-claim-list']"))))
+survClaimList_link.click()
+ToasterPopupClick()
+logOut()
---
+++ MotorClaim_ICGMS/_16_MotorClaim_BillVerification.py
@@ -0,0 +1,169 @@
+import time
+from Generic.BaseClassICGMS import *
+from Generic.Login_ICGMS import *
+from selenium.webdriver.common.by import By
+
+# Login
+setUp()
+time.sleep(1)
+Login_ICGMS.login(Surveyor_Email1 , P2ass)
+
+ToasterPopupClick()
+time.sleep(0.5)
+
+# Open Surveyor Claim View List
+survClaimList_link = wait.until((EC.presence_of_element_located((By.XPATH, "//a[@href='/surveyor-claim-list']"))))
+survClaimList_link.click()
+
+# Click on Bill Verification button
+billVer_button = wait.until((EC.presence_of_element_located((By.XPATH, "(//button[text()='Bill Verification'])[1]"))))
+billVer_button.click()
+
+# View Inspection Report
+viewInspecReport_button = wait.until((EC.presence_of_element_located((By.XPATH, "(//button[text()='View'])[1]"))))
+viewInspecReport_button.click()
+ToasterPopupClick()
+
+# Close Inspection Report
+close_button = driver.find_element(By.XPATH, "//button[@aria-label='Close']")
+close_button.click()
+
+# Download Inspection Report
+DownCustReport = driver.find_element(By.XPATH, "(//button[text()='Download'])[1]")
+DownCustReport.click()
+ToasterPopupClick()
+time.sleep(1)
+
+# View Inspection Key Report
+viewInspecReport_button = driver.find_element(By.XPATH, "(//button[text()='View'])[2]")
+viewInspecReport_button.click()
+ToasterPopupClick()
+
+# Close Inspection Key Report
+close_button = driver.find_element(By.XPATH, "//button[@aria-label='Close']")
+close_button.click()
+
+# Download Inspection Key Report
+DownCustReport = driver.find_element(By.XPATH, "(//button[text()='Download'])[2]")
+DownCustReport.click()
+ToasterPopupClick()
+time.sleep(1)
+
+# View Repairer Bill Doc
+viewInspecReport_button = driver.find_element(By.XPATH, "(//button[text()='View'])[3]")
+viewInspecReport_button.click()
+ToasterPopupClick()
+
+# Close Repairer Bill Doc
+close_button = driver.find_element(By.XPATH, "//button[@aria-label='Close']")
+close_button.click()
+
+# Download Repairer Bill Doc
+DownCustReport = driver.find_element(By.XPATH, "(//button[text()='Download'])[3]")
+DownCustReport.click()
+ToasterPopupClick()
+time.sleep(1)
+
+# View Other Bill Doc
+viewInspecReport_button = driver.find_element(By.XPATH, "(//button[text()='View'])[4]")
+viewInspecReport_button.click()
+ToasterPopupClick()
+
+# Close Other Bill Doc
+close_button = driver.find_element(By.XPATH, "//button[@aria-label='Close']")
+close_button.click()
+
+# Download Other Bill Doc
+DownCustReport = driver.find_element(By.XPATH, "(//button[text()='Download'])[4]")
+DownCustReport.click()
+ToasterPopupClick()
+time.sleep(1)
+
+# View Final Bill Doc
+viewInspecReport_button = driver.find_element(By.XPATH, "(//button[text()='View'])[5]")
+viewInspecReport_button.click()
+ToasterPopupClick()
+
+# Close Final Bill Doc
+close_button = driver.find_element(By.XPATH, "//button[@aria-label='Close']")
+close_button.click()
+
+# Download Final Bill Doc
+DownCustReport = driver.find_element(By.XPATH, "(//button[text()='Download'])[5]")
+DownCustReport.click()
+ToasterPopupClick()
+time.sleep(1)
+
+# View Surveyor Survey Bill Doc
+viewInspecReport_button = driver.find_element(By.XPATH, "(//button[text()='View'])[6]")
+viewInspecReport_button.click()
+ToasterPopupClick()
+
+# Close Surveyor Survey Bill Bill Doc
+close_button = driver.find_element(By.XPATH, "//button[@aria-label='Close']")
+close_button.click()
+
+# Download Surveyor Survey Bill Bill Doc
+DownCustReport = driver.find_element(By.XPATH, "(//button[text()='Download'])[6]")
+DownCustReport.click()
+ToasterPopupClick()
+time.sleep(1)
+
+# View Optional Bill Doc
+viewInspecReport_button = driver.find_element(By.XPATH, "(//button[text()='View'])[7]")
+viewInspecReport_button.click()
+ToasterPopupClick()
+
+# Close Optional Bill Doc
+close_button = driver.find_element(By.XPATH, "//button[@aria-label='Close']")
+close_button.click()
+
+# Download Optional Bill Doc
+DownCustReport = driver.find_element(By.XPATH, "(//button[text()='Download'])[7]")
+DownCustReport.click()
+ToasterPopupClick()
+time.sleep(1)
+
+# View Surveyor Other Bill Doc
+viewInspecReport_button = driver.find_element(By.XPATH, "(//button[text()='View'])[8]")
+viewInspecReport_button.click()
+ToasterPopupClick()
+
+# Close Surveyor Other Bill Doc
+close_button = driver.find_element(By.XPATH, "//button[@aria-label='Close']")
+close_button.click()
+
+# Download Surveyor Other Bill Doc
+DownCustReport = driver.find_element(By.XPATH, "(//button[text()='Download'])[8]")
+DownCustReport.click()
+ToasterPopupClick()
+time.sleep(1)
+
+# View Customer Inspection Doc
+viewInspecReport_button = driver.find_element(By.XPATH, "//label[text()='Customer Inspection report']/following-sibling::button[contains(text(),'View')]")
+viewInspecReport_button.click()
+ToasterPopupClick()
+
+# Close Customer Inspection Doc
+close_button = driver.find_element(By.XPATH, "//button[@aria-label='Close']")
+close_button.click()
+
+# Download Customer Inspection Doc
+DownCustReport = driver.find_element(By.XPATH, "//label[text()='Customer Inspection report']/following-sibling::button[contains(text(),'Download')]")
+DownCustReport.click()
+ToasterPopupClick()
+time.sleep(1)
+
+# Enter Remark
+remark_text = driver.find_element(By.XPATH, "//textarea[@placeholder='Enter Remark']")
+remark_text.send_keys("All bills viewed and downloaded successfully")
+
+# Click on Approve Button
+approve_button = driver.find_element(By.XPATH, "//span[text()='Approve']")
+approve_button.click()
+
+# Open Surveyor Claim View List
+survClaimList_link = wait.until((EC.presence_of_element_located((By.XPATH, "//a[@href='/surveyor-claim-list']"))))
+survClaimList_link.click()
+ToasterPopupClick()
+logOut()
